528bec4572d97a994b00177e28a9e1f72f40f35c
Bharath964 <122285454+Bharath964@users.noreply.github.com>
2025-05-05 21:55:49 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,7 @@ unset POETRY_VIRTUALENVS_CREATE
 # Install Python 3.10
 pyenv install 3.10.13
 pyenv global 3.10.13
+# Force Python version (if available)
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirementsdeploy.txt --no-cache-dir
 
-# Now use python3.10 explicitly
-python -m pip install --upgrade pip
-python -m pip install -r requirementsdeploy.txt
-python -m pip install streamlit==1.15.2
