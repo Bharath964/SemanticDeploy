27bafbeed8c65e6a09aeb735ffb10b527088a88c
Bharath964 <122285454+Bharath964@users.noreply.github.com>
2025-05-05 22:04:57 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-# Disable Poetry
-unset PIP_REQUIRE_VIRTUALENV
-unset POETRY_VIRTUALENVS_CREATE
 # Install Python 3.10
-pyenv install 3.10.13
-pyenv global 3.10.13
-# Force Python version (if available)
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirementsdepoly.txt --no-cache-dir
-
+sudo apt-get update
+sudo apt-get install python3.10
+python3.10 -m pip install --upgrade pip
+python3.10 -m pip install -r requirementsdeploy.txt
