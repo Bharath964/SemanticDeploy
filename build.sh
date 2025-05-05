12623715dad4311e4d2681b3f35f41b2781ef84f
Bharath964 <122285454+Bharath964@users.noreply.github.com>
2025-05-05 21:59:07 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ pyenv install 3.10.13
 pyenv global 3.10.13
 # Force Python version (if available)
 python3 -m pip install --upgrade pip
-python3 -m pip install -r requirementsdeploy.txt --no-cache-dir
+python3 -m pip install -r requirementsdepoly.txt --no-cache-dir
 
